4f5803995189066b10d87be1fdb6f374871d8ba2
zhangchi <zhangchi@mi-ya.com.cn>
2021-08-06 11:18:23 +0800

    feat: 初始化项目，当做main分支，其余分支各自命名，进行各自的职责划分；

---
+++ src/main/java/com/miya/ZccTempApp.java
@@ -0,0 +1,13 @@
+package com.miya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZccTempApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZccTempApp.class, args);
+    }
+
+}
---
+++ src/test/java/com/miya/ZccTempAppTests.java
@@ -0,0 +1,13 @@
+package com.miya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZccTempAppTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
