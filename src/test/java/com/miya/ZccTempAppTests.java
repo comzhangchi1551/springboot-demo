03fb54bf60d90918168a3d2a9fabecb21b58f0cc
zhangchi <351163675@qq.com>
2022-09-23 09:57:58 +0800

    redisson限流器代码模板；

--- src/test/java/com/miya/ZccTempAppTests.java
+++ src/test/java/com/miya/ZccTempAppTests.java
@@ -1,10 +1,11 @@
 package com.miya;
 
-import com.miya.dao.TempUserMapper;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
-import org.redisson.api.RBucket;
+import org.redisson.api.RRateLimiter;
+import org.redisson.api.RateIntervalUnit;
+import org.redisson.api.RateType;
 import org.redisson.api.RedissonClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -15,24 +16,27 @@ import java.util.concurrent.TimeUnit;
 @Slf4j
 class ZccTempAppTests {
 
-
-    @Autowired
-    private TempUserMapper tempUserMapper;
-
     @Autowired
     private RedissonClient redissonClient;
 
     @SneakyThrows
     @Test
     public void contextLoads() {
-        RBucket<Object> bucket = redissonClient.getBucket("k1");
-        bucket.set("7787", 100, TimeUnit.SECONDS);
-
         for (int i = 0; i < 10; i++) {
-            TimeUnit.SECONDS.sleep(3);
-            RBucket<Object> k1 = redissonClient.getBucket("k1");
-            long expireTime = k1.getExpireTime();
-            System.out.println(expireTime);
+            // 1. 声明一个限流器；
+            RRateLimiter abc = redissonClient.getRateLimiter("abcd");
+
+            // 2. 如果这个限流器不存在，则在redis服务中添加该限流器；
+            if (!abc.isExists()) {
+
+                // 2.1 设置为一秒钟内发放5个令牌；
+                abc.setRate(RateType.OVERALL, 5, 1, RateIntervalUnit.SECONDS);
+            }
+
+            // 3. 试图获取一个令牌，获取到返回true；
+            if (abc.tryAcquire(30, TimeUnit.SECONDS)) {
+                System.out.println(System.currentTimeMillis());
+            }
         }
     }
 
