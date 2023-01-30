88b321fe5fde8c1003d3eaf85a4db7d8acf7c784
zhangchi <351163675@qq.com>
2023-01-30 13:41:53 +0800

    测试提交

--- src/main/java/com/miya/ZccTempApp.java
+++ src/main/java/com/miya/ZccTempApp.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ZccTempApp {
 
     public static void main(String[] args) {
+        // 测试上传
         SpringApplication.run(ZccTempApp.class, args);
     }
 
