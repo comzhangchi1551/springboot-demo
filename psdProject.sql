337a17855a325cc6af3d4daedb6289805f37ee65
zhangchi <351163675@qq.com>
2023-04-21 17:18:55 +0800

    sql备份

--- psdProject.sql
+++ psdProject.sql
@@ -1,3 +1,43 @@
+create table binding_layer_relation
+(
+    id         bigint auto_increment comment '主键id'
+        primary key,
+    binding_id bigint            not null,
+    layer_id   bigint            not null,
+    del_flag   tinyint default 0 not null
+)
+    comment '绑定和图层的关联中间表';
+
+create table group_layer_relation
+(
+    id       bigint auto_increment
+        primary key,
+    group_id bigint            not null comment 'layer_group表的主键id',
+    layer_id bigint            not null comment 'layer表的主键id',
+    del_flag tinyint default 0 not null
+)
+    comment '分组和图层中间表';
+
+create table psd_binding
+(
+    id             bigint auto_increment comment '主键id'
+        primary key,
+    psd_project_id bigint                  not null comment 'psd_project表的主键id',
+    binding_name   varchar(255) default '' not null comment '绑定的名称',
+    del_flag       tinyint      default 0  not null comment '删除标记，0：未删除，1：已删除；'
+);
+
+create table psd_group
+(
+    id             bigint auto_increment
+        primary key,
+    psd_project_id bigint                  not null comment 'psd_project表的主键id',
+    group_name     varchar(255) default '' not null comment '分组名称',
+    group_sort     int unsigned default 0  not null comment '分组排序',
+    del_flag       tinyint      default 0  not null
+)
+    comment '图层分组表';
+
 create table psd_project
 (
     id           bigint auto_increment comment '主键id'
