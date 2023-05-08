392068bb23b4e2b023bd7ca7757eca218fa6bae4
zhangchi <351163675@qq.com>
2023-05-08 08:53:51 +0800

    整理配置文件

--- src/test/java/com/miya/ZccTempAppTests.java
+++ src/test/java/com/miya/ZccTempAppTests.java
@@ -1,22 +1,27 @@
 package com.miya;
 
-import com.miya.dao.TempUserMapper;
+import com.miya.entity.model.TempUser;
+import com.miya.service.TempUserService;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
+
 @SpringBootTest
 @Slf4j
 class ZccTempAppTests {
 
     @Autowired
-    private TempUserMapper tempUserMapper;
+    private TempUserService tempUserService;
 
     @SneakyThrows
     @Test
     void contextLoads() {
+        TempUser detailById = tempUserService.getDetailById(1L);
+        System.out.println(detailById);
     }
 
 
