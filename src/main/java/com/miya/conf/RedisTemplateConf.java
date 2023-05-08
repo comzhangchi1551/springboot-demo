ab815cdb5fe2bdeb699f628ef16e7e2c21619f4f
zhangchi <351163675@qq.com>
2023-05-08 09:02:24 +0800

    优化redisTemp配置序列化；

--- src/main/java/com/miya/conf/RedisTemplateConf.java
+++ src/main/java/com/miya/conf/RedisTemplateConf.java
@@ -8,6 +8,7 @@ import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
@@ -16,21 +17,14 @@ public class RedisTemplateConf {
 
     @Autowired
     public RedisTemplateConf(RedisTemplate redisTemplate) {
-        Jackson2JsonRedisSerializer<Object> objectJackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer<>(Object.class);
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
-        objectMapper.activateDefaultTyping(LaissezFaireSubTypeValidator.instance, ObjectMapper.DefaultTyping.NON_FINAL, JsonTypeInfo.As.WRAPPER_ARRAY);
-
-        objectJackson2JsonRedisSerializer.setObjectMapper(objectMapper);
 
         StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
-
+        GenericJackson2JsonRedisSerializer valueSerializer = new GenericJackson2JsonRedisSerializer();
         redisTemplate.setKeySerializer(stringRedisSerializer);
-        redisTemplate.setValueSerializer(objectJackson2JsonRedisSerializer);
+        redisTemplate.setValueSerializer(valueSerializer);
 
         redisTemplate.setHashKeySerializer(stringRedisSerializer);
-        redisTemplate.setHashValueSerializer(objectJackson2JsonRedisSerializer);
-
+        redisTemplate.setHashValueSerializer(valueSerializer);
         redisTemplate.afterPropertiesSet();
     }
 }
--- src/test/java/com/miya/ZccTempAppTests.java
+++ src/test/java/com/miya/ZccTempAppTests.java
@@ -1,22 +1,33 @@
 package com.miya;
 
 import com.miya.dao.TempUserMapper;
+import com.miya.entity.model.TempUser;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 
 @SpringBootTest
 @Slf4j
 class ZccTempAppTests {
 
     @Autowired
-    private TempUserMapper tempUserMapper;
+    private RedisTemplate redisTemplate;
 
     @SneakyThrows
     @Test
     void contextLoads() {
+        ValueOperations valueOperations = redisTemplate.opsForValue();
+        TempUser tempUser = new TempUser();
+        tempUser.setId(32L);
+        tempUser.setUsername("zhangchi");
+        tempUser.setAge(14);
+        valueOperations.set("大傻逼", tempUser);
+        Object 大傻逼 = valueOperations.get("大傻逼");
+        System.out.println(大傻逼);
     }
 
 
