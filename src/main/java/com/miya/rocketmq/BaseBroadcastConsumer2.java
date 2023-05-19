3455c17022f60e9d3cf1ce6d5c3e657b2f32d864
zhangchi <351163675@qq.com>
2023-05-19 23:51:43 +0800

    广播模式demo

--- src/main/java/com/miya/rocketmq/BaseBroadcastConsumer2.java
+++ src/main/java/com/miya/rocketmq/BaseBroadcastConsumer2.java
@@ -6,19 +6,22 @@ import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyContext;
 import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
 import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently;
 import org.apache.rocketmq.common.message.MessageExt;
+import org.apache.rocketmq.common.protocol.heartbeat.MessageModel;
 
 import java.util.List;
 
-public class BaseConsumer {
+public class BaseBroadcastConsumer2 {
     @SneakyThrows
     public static void main(String[] args) {
         // 创建消费者对象
-        DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("my-producer-group1");
+        DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("my-broadcast-producer-group1");
         // 设置nameServer地址
         consumer.setNamesrvAddr("192.168.31.208:9876");
 
         // 订阅主题：topic；和过滤消息用的tag表达式；
-        consumer.subscribe("MyTopic1", "*");
+        consumer.subscribe("MyBroadcastTopic", "*");
+
+        consumer.setMessageModel(MessageModel.BROADCASTING);
 
         // 创建一个监听器，当broker把消息推过来时，调用；
         consumer.registerMessageListener(new MessageListenerConcurrently() {
