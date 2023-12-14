a7a60dafc1d3fdeb653363da5a1510b781ec7f9f
zhangchi <c.zhang25@trip.com>
2023-12-15 01:49:50 +0800

    event test

--- src/main/java/com/miya/entity/model/TempUser.java
+++ src/main/java/com/miya/entity/model/TempUser.java
@@ -4,8 +4,10 @@ import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import lombok.Data;
 
+import java.io.Serializable;
+
 @Data
-public class TempUser {
+public class TempUser implements Serializable {
     @TableId(type = IdType.AUTO)
     private Long id;
 
