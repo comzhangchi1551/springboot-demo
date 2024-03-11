411acb0d32d8e10ab2ef9825280bd0c0d4649702
zhangchi <c.zhang25@trip.com>
2024-03-11 10:42:41 +0800

    调整

--- src/main/java/com/miya/MainTest.java
+++ src/main/java/com/miya/MainTest.java
@@ -1,10 +1,29 @@
 package com.miya;
 
+import com.google.common.collect.Lists;
+import com.miya.entity.model.TempUser;
+
+import java.util.List;
+
 public class MainTest {
 
     public static void main(String[] args) {
 
-        int a = 10 / 0;
-        System.out.println("a = " + a);
     }
+
+
+    public static TempUser getTempUser() {
+        return new TempUser(1l, "eee", 21);
+    }
+
+
+    public static List<TempUser> getTempUserList() {
+        return Lists.newArrayList(
+                new TempUser(1l, "eee", 21),
+                new TempUser(2l, "dd", 44),
+                new TempUser(3l, "aaa", 13),
+                new TempUser(4l, "fff", 13)
+        );
+    }
+
 }
